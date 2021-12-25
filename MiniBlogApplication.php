d1d24ed25c850f593dc42028ff68548c27905d2c
Kuma-inataku <inataku0121@gmail,com>
2021-12-25 12:42:01 +0900

    アプリの作成

--- MiniBlogApplication.php
+++ MiniBlogApplication.php
@@ -1,1 +1,27 @@
 <?php
+
+class MiniBlogApplication extends Application
+{
+    protected $login_action = ['account', 'signin'];
+
+    public function getRootDir()
+    {
+      return dirname(__FILE__);
+    }
+
+    protected function registerRoutes()
+    {
+      return [
+
+      ];
+    }
+
+    protected function configure()
+    {
+      $this->db_manager->connect('master', [
+        'dsn' => 'mysql:dbname=mini_blog;host=localhost',
+        'user' => 'root',
+        'password' => 'root123',
+      ]);
+    }
+}
