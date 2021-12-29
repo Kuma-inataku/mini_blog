acc29c8d1d3d197e1ca29b76e5d2072643b5b2fc
Kuma-inataku <inataku0121@gmail,com>
2021-12-29 09:47:44 +0900

    indexメソッドの追加

---
+++ controllers/StatusController.php
@@ -0,0 +1,18 @@
+<?php
+
+class StatusController extends Controller
+{
+  public function indexAction()
+  {
+    // TODO: 'u'大文字？
+    $user = $this->session->get('user');
+    $statuses = $this->db_manager->get('Status')->fetchAllPersonalArchivesByUserId($user['id']);
+
+    return $this->render([
+      'statuses' => $statuses,
+      'body' => '',
+      // TODO: post?
+      '_token' => $this->generateCsrfToken('status/post'),
+    ]);
+  }
+}
