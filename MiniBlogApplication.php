99d417fb4f1a303ff1877a1b808ea126e49bebc0
Kuma-inataku <inataku0121@gmail,com>
2021-12-29 12:09:12 +0900

    ユーザー情報、投稿情報表示機能＆そののリンク、

--- MiniBlogApplication.php
+++ MiniBlogApplication.php
@@ -12,10 +12,17 @@ class MiniBlogApplication extends Application
     protected function registerRoutes()
     {
         return [
+            //Statusのルーティング
             '/'
                 => array('controller' => 'status', 'action' => 'index'),
             '/status/post'
                 => array('controller' => 'status', 'action' => 'post'),
+            '/user/:user_name'
+                => array('controller' => 'status', 'action' => 'user'),
+            '/user/:user_name/status/:id'
+                => array('controller' => 'status', 'action' => 'show'),
+
+            // Accountのルーティング
             '/account'
                 => array('controller' => 'account', 'action' => 'index'),
             '/account/:action'
