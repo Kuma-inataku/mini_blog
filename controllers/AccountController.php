aca6cb0cc9490f978d4dbacbb58bc4e512f71162
Kuma-inataku <inataku0121@gmail,com>
2022-01-04 11:18:54 +0900

    ログアウト、ビューファイルの修正

--- controllers/AccountController.php
+++ controllers/AccountController.php
@@ -138,4 +138,12 @@ class AccountController extends Controller
       '_token' => $this->generateCsrfToken('account/signup')
     ], 'signup');
   }
+
+  public function signoutAction()
+  {
+    $this->session->clear();
+    $this->session->setAuthenticated(false);
+    
+    return $this->redirect('/account/signin');
+  }
 }
